1215f9983b29402012dc8a7ba01569c5027bac58
Akito Mizuno <mznakt@gmail.com>
2018-07-31 16:01:26 +0900

    refactor install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,18 @@ set -u
 DOT_DIRECTORY="${HOME}/src/dotfiles"
 cd ${DOT_DIRECTORY}
 
-echo "start making link"
+echo "start making link..."
 echo
 
 # ./ ../ を除いて .から始まるファイルを対象にループ
-for f in .??*
-do
+for file in .??* ; do
     # シンボリックリンクリンクを作成しないファイル
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-    [[ "$f" == ".zshrc_old" ]] && continue
+    [[ "$file" == ".git" ]] && continue
+    [[ "$file" == ".DS_Store" ]] && continue
+    [[ "$file" == ".zshrc_old" ]] && continue
 
-    ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
-    echo "$f"
+    ln -snfv ${DOT_DIRECTORY}/${file} ${HOME}/${file}
+    echo "$file"
 done
 
 echo
